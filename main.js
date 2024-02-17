415af7c496f0a54443dac23e487b3de610abcd36
Waheed-Rao <108606703+Waheed-Rao@users.noreply.github.com>
2024-02-17 07:20:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Assignment1
+//console.log('hello world');
+//Assigment2
+//var myName = "Waheed";
+//var message = "is a nice person";
+//console.log(myName + " " + message);
---
+++ main.ts
@@ -0,0 +1,7 @@
+//Assignment1
+//console.log ('hello world');
+
+//Assigment2
+//let myName = "Waheed";
+//let message = "is a nice person";
+//console.log (myName+" "+message);
